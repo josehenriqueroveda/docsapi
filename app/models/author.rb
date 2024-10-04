13cc14fb5124ee3ac8cfc2aa2a52bf01e52604b9
josehenriqueroveda <josehenriqueroveda.dev@gmail.com>
2024-10-04 10:40:57 -0300

    feat: Add validations and relationships closes #3

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+  has_many :documents
+
+  validates :name, uniqueness: true, presence: true
+  validates :department, presence: true
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,13 @@
 class Document < ApplicationRecord
   belongs_to :doc_shelf
   belongs_to :author
+
+  validates :title, :version, :status, :url, :published_at, presence: true
+  validate :is_shelf_on_limit
+
+  def is_shelf_on_limit
+    if self.doc_shelf.documents.count >= self.doc_shelf.limit
+      erros.add(:base, "Document limit reached")
+    end
+  end
 end
